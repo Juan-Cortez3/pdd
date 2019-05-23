752a76746b89bd4cd78fc8a6d6e8552048c9cd15
Juan-Cortez3 <juancortez0128@gmail.com>
2019-05-23 09:03:30 +0000

    Add shebang for autogen.sh
    Description:
    ...

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+### autogen.sh - tool to help build pdd from a repository checkout
+
 aclocal     # set up an m4 environment
 autoconf    # generate configure from configure.ac
 automake --add-missing # Generate Makefile.in from Makefile.am
